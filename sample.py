9ce320f741ab3b84277425455eca8145c0e26ecb
Takumi <takumikobayashi0708@gmail.com>
2019-01-04 16:43:51 +0900

    挙動確認用のPythonファイルを追加

---
+++ sample.py
@@ -0,0 +1,13 @@
+# classを使ってJSONファイルを処理してみる
+
+from CiNii import CiNii
+import json
+import requests
+
+
+res = requests.get('https://ci.nii.ac.jp/naid/40021719192.json')
+json_d = json.loads(res.text)
+
+data = CiNii()
+title = data.get_article_title_from_dict(json_d)
+print(title)
